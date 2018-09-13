18e1b5a2bd4f8a5abff6fa67806c9ab55236d8e1
ARBhosale <anirsk@gmail.com>
2018-09-13 15:09:54 -0700

    formatting all tasks and folders to load in treeView

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -78,8 +78,8 @@ doOnLoad = () => {
         }
     });
 
-    addToDo.attachEvent("onButtonClick", function(name, command){
-        if(name=="addToDoBtn"){
+    addToDo.attachEvent("onButtonClick", function (name, command) {
+        if (name == "addToDoBtn") {
             var values = addToDo.getFormData();
             console.log(values);
             var newData;
@@ -95,12 +95,12 @@ doOnLoad = () => {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body:JSON.stringify(newData)
+                body: JSON.stringify(newData)
             });
-            fetch(request).then((response)=>{
+            fetch(request).then((response) => {
                 console.log(response);
             }
-                
+
             ).catch()
 
         }
@@ -109,12 +109,7 @@ doOnLoad = () => {
 
 
     // Creating a Tree View.
-    directoryTreeView = layout.cells("a").attachTreeView({
-        json: "data/directory.json",
-        iconset: "font_awesome",
-        dnd:true
-    });
-
+    loadDirectory(layout);
 
 };
 
@@ -131,8 +126,11 @@ function initializeGrid(layout) {
     loadAllTasks(mygrid);
 }
 
+const TASKS_API_URL = "http://localhost:8080/task/v1";
+const DIRECTORY_URL = TASKS_API_URL + '/directory';
+
 function loadAllTasks(grid) {
-    var allTasksApiUrl = "http://localhost:8080/task/v1";
+    var allTasksApiUrl = TASKS_API_URL;
     var tasksData = { rows: [] };
     fetch(allTasksApiUrl)
         .then((allTasksResponse) => {
@@ -140,14 +138,143 @@ function loadAllTasks(grid) {
         })
         .then(function (allTasks) {
             if (allTasks && allTasks.length > 0) {
-                allTasks.forEach((task)=>{
-                    tasksData.rows.push({id: task.id, data:[task.name, task.description, task.status]});
+                allTasks.forEach((task) => {
+                    tasksData.rows.push({ id: task.id, data: [task.name, task.description, task.status] });
                 });
                 mygrid.parse(tasksData, "json"); //takes the name and format of the data source
             }
         })
-        .catch((error) => { console.error(error); });    
+        .catch((error) => { console.error(error); });
 }
 
+function loadDirectory(layout) {
+    var directoryApiUrl = DIRECTORY_URL;
+
+
+
+    fetch(directoryApiUrl)
+        .then((directoryResponse) => { return directoryResponse.json(); })
+        .then((directory) => {
+            console.log(directory);
+
+            let constructedDirectory = constructDirectory(directory);
+            directoryTreeView = layout.cells("a").attachTreeView({
+                items: constructedDirectory,
+                iconset: "font_awesome",
+                dnd: true
+            });
+        })
+        .catch((error) => { console.error(error); });
+
+}
+
+function constructDirectory(directoryData) {
+    let directory = [];
+    let folderMap = getFolderMap(directoryData);
+    addTasksToFolders(folderMap, directoryData.allTasks);
+
+    let rootFolder = getRootFolder(directoryData);
+    if (folderMap.has(rootFolder.id)) {
+        return [getDirectoryItemFromFolder(folderMap, rootFolder.id)];
+    } else {
+        return [];
+    }
+
+}
+
+function getRootFolder(directoryData) {
+    if (!directoryData || !directoryData.allFolders) {
+        return null;
+    }
+    let allFolders = directoryData.allFolders;
+    for (let i = 0; i < allFolders.length; i++) {
+        if (allFolders[i].parentFolderId == null) {
+            return allFolders[i];
+        }
+    }
+    return null;
+}
+
+function getFolderMap(directoryData) {
+    let folderMap = new Map();
+    if (!directoryData || !directoryData.allFolders) {
+        return folderMap;
+    }
+    let allFolders = directoryData.allFolders;
+    for (let i = 0; i < allFolders.length; i++) {
+        let folder = allFolders[i];
+
+        if (!folderMap.has(folder.id)) {
+            folderMap.set(folder.id, { folder: folder, children: [] });
+        }
+
+        let parentFolderId = folder.parentFolderId;
+
+        if (null != parentFolderId) {
+            if (folderMap.has(parentFolderId)) {
+                let childrenElements = folderMap.get(parentFolderId).children;
+                childrenElements.push(folder);
+            }
+        }
+
+    }
+    return folderMap;
+}
+
+function addTasksToFolders(folderMap, allTasks) {
+    for (let i = 0; i < allTasks.length; i++) {
+        let task = allTasks[i];
+        if (folderMap.has(task.folderId)) {
+            let childrenElements = folderMap.get(task.folderId).children;
+            childrenElements.push(task);
+        }
+    }
+}
+
+function getDirectoryItemFromFolder(folderMap, folderId) {
+    if (!folderMap.has(folderId)) {
+        return null;
+    }
+    let folderData = folderMap.get(folderId);
+
+    let item = {
+        id: 'f' + folderData.folder.id,
+        text: folderData.folder.name,
+        kids: folderData.children.length > 0,
+        open: folderData.folder.parentFolderId == null ? 0 : 1,
+        items: getDirectoryItems(folderData)
+    }
+    return item;
+}
+
+function getDirectoryItemFromTask(task) {
+    let item = {
+        id: 't' + task.id,
+        text: task.name,
+        kids: false,
+        open: 1
+    };
+    return item;
+}
+
+function getDirectoryItems(folderData) {
+    let items = [];
+    for (let i = 0; i < folderData.children.length; i++) {
+        let item = folderData.children[i];
+        let directoryItem;
+        if (isFolder(item)) {
+            directoryItem = getDirectoryItemFromFolder(item);
+        } else {
+            directoryItem = getDirectoryItemFromTask(item);
+        }
+        items.push(directoryItem);
+    }
+    return items;
+}
+
+function isFolder(item) {
+    // folders have the field parentFolderId
+    return item.parentFolderId != undefined;
+}
 
 

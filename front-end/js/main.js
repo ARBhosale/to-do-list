a398c1024e90f2b45711f9b5892702d78984fce9
Jay Palan <jpalan.gmu@gmail.com>
2018-09-13 15:57:03 -0700

    On Select of To Do and creating a model

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,7 +1,8 @@
 var layout;
 var mygrid;
-var tasks;
+var directory;
 doOnLoad = () => {
+    fetchAllData();
     // Creating a layout 
     layout = new dhtmlXLayoutObject({
         parent: document.getElementById("layoutObj"),
@@ -53,24 +54,21 @@ doOnLoad = () => {
     layout.cells("a").attachObject("mainDirectory");
     layout.cells("b").attachObject("toDoList");
     layout.cells("c").attachObject("gridbox");
-    initializeGrid(layout);
-
-    // Adding a toolbox for adding a To Do or a Folder
-
     var toDoToolbar = new dhtmlXToolbarObject("toolbarObj");
     toDoToolbar.addButton("addToDo", 1, "Add a To Do", "", ""); // addButton(id,pos,text,imgEnabled,imgDisabled); 
     toDoToolbar.addSeparator("seperator1", 2);
     toDoToolbar.addButton("addFolder", 3, "Add a Folder", "", "");
 
-
     // Add To Do Window
-    addToDo = layout.cells("d").attachForm();
+    var addToDo = layout.cells("d").attachForm();
     addToDo.loadStruct("data/addToDo.xml");
+    showToDoForm(addToDo)
     toDoToolbar.attachEvent("onClick", function (id) {
         if (id === "addToDo") {
             console.log("add to button clicked");
             addToDo = layout.cells("d").attachForm();
             addToDo.loadStruct("data/addToDo.xml");
+            showToDoForm(addToDo)
 
         } else if (id === "addFolder") {
             addFolder = layout.cells("d").attachForm();
@@ -80,6 +78,87 @@ doOnLoad = () => {
         }
     });
 
+    
+    // Creating a Tree View.
+    directoryTreeView = layout.cells("a").attachTreeView({
+        json: "data/directory.json",
+        iconset: "font_awesome",
+        dnd:true
+    });
+
+
+};
+
+function fetchAllData(){
+    var allTasksApiUrl = "http://localhost:8080/task/v1/directory";
+    fetch(allTasksApiUrl)
+        .then((allDataResponse) => {
+            return allDataResponse.json();
+        })
+        .then((allData)=>{
+            directory = allData;
+            console.log("All the data is loaded from the server");
+            initializeGrid(layout);
+            
+        }).catch((error)=>console.log(error))
+        
+        
+}
+
+function initializeGrid(layout) {
+    mygrid = new dhtmlXGridObject('gridbox');
+    //the path to images required by grid 
+    mygrid.setImagePath("./codebase/imgs/");
+    mygrid.setHeader("Name,Description,Status");//the headers of columns  
+    mygrid.setInitWidths("100,400,100");          //the widths of columns  
+    mygrid.setColAlign("center,center,center");
+    mygrid.attachEvent("onRowSelect",doOnRowSelected);       //the alignment of columns   
+    mygrid.setColTypes("ro,ed,ed");                //the types of columns  
+    
+    mygrid.init();//finishes initialization and renders the grid on the page 
+    loadAllTasks(mygrid);
+
+}   
+
+function loadAllTasks(grid) {
+    var tasksData = { rows: [] };
+    var allTasks=directory.allTasks;
+    allTasks.forEach((task)=>{
+        tasksData.rows.push({
+            id: task.id, 
+            data:[
+                task.name, 
+                task.description,
+                task.status
+            ]
+        });
+    });
+    mygrid.parse(tasksData, "json");
+   
+}
+
+function doOnRowSelected(id){
+    var allTasks= directory.allTasks;
+    var selectedTask = allTasks.filter(task => task.id == id);
+    console.log(selectedTask);
+    // layout.cells("d").detachForm();
+    editToDo = layout.cells("d").attachForm();
+    formData = [
+        {type:"fieldset", name:"data", label:"Edit To Do", inputWidth:"auto", 
+         list:[
+            {type:"input",offsetLeft:"15",    name:'editToDo', label:'Name', value: `${selectedTask[0].name}`},
+            {type:"input",offsetLeft:"15", name:"editToDoDescription", label:"Description", value: `${selectedTask[0].description}`},
+            {type:"input",offsetLeft:"15", name:"editToDoStatus", label:"Status", readonly:true, value: `${selectedTask[0].status}`},
+            {type:"button",   name:"completeTask", value:"Complete"},
+            {type:"button",   name:"deleteTask", value:"Delete"}] 
+        }
+    ]
+    editToDo.loadStruct(formData);
+
+}
+
+
+function showToDoForm(addToDo){
     addToDo.attachEvent("onButtonClick", function(name, command){
         if(name=="addToDoBtn"){
             var values = addToDo.getFormData();
@@ -103,65 +182,14 @@ doOnLoad = () => {
             .then((response)=>{
                 return response.json();
             })
-            .then((data)=>{ 
+            .then((data)=>{
+                console.log(data);
+                directory.allTasks.push(data);
+                console.log(directory);
                 loadAllTasks(mygrid);
             }).catch((error)=>{console.log(error);})
 
         }
     });
 
-    // Creating a Tree View.
-    directoryTreeView = layout.cells("a").attachTreeView({
-        json: "data/directory.json",
-        iconset: "font_awesome",
-        dnd:true
-    });
-
-
-};
-
-function initializeGrid(layout) {
-    mygrid = new dhtmlXGridObject('gridbox');
-    //the path to images required by grid 
-    mygrid.setImagePath("./codebase/imgs/");
-    mygrid.setHeader("Name,Description");//the headers of columns  
-    mygrid.setInitWidths("100,450");          //the widths of columns  
-    mygrid.setColAlign("center,center");
-    mygrid.attachEvent("onRowSelect",doOnRowSelected);       //the alignment of columns   
-    mygrid.setColTypes("ro,ed");                //the types of columns  
-    mygrid.init();      //finishes initialization and renders the grid on the page 
-    loadAllTasks(mygrid);
 }
-
-function loadAllTasks(grid) {
-    var allTasksApiUrl = "http://localhost:8080/task/v1";
-    var tasksData = { rows: [] };
-    fetch(allTasksApiUrl)
-        .then((allTasksResponse) => {
-            return allTasksResponse.json();
-        })
-        .then(function (allTasks) {
-            if (allTasks && allTasks.length > 0) {
-                allTasks.forEach((task)=>{
-                    tasksData.rows.push({
-                        id: task.id, 
-                        data:[
-                            task.name, 
-                            task.description
-                        ]
-                    });
-                });
-                mygrid.parse(tasksData, "json"); //takes the name and format of the data source
-            }
-        })
-        .catch((error) => { console.error(error); });    
-}
-
-function doOnRowSelected(id){
-    console.log(id);
-
-}
-
-
-
-
